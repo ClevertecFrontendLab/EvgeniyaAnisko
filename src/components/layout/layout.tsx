bc2882485b6dd863ab54220d9c9647ddde49d207
Evgeniya Anisko <evgeniya.anisko@gmail.com>
2024-02-09 16:46:42 +0300

    feat: add exit and collapsed buttons

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import './layout.css';
 import {
   CalendarOutlined,
   HeartFilled,
   TrophyFilled,
-  IdcardOutlined
+  IdcardOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Layout, Menu } from 'antd';
+import { Button, Layout, Menu } from 'antd';
 import { Outlet } from 'react-router-dom';
-import { CleverIcon, FitIcon } from '@components/logo';
+import { CleverIcon, ExitIcon, FitIcon } from '@components/icons';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Content, Footer, Sider } = Layout;
 
 const labels = ['Календарь', 'Тренировки', 'Достижения', 'Профиль'];
 
@@ -30,9 +32,20 @@ const items: MenuProps['items'] = [
   },
 }));
 
-const LayoutPage: React.FC = () => (
-  <Layout hasSider>
+const LayoutPage: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return <Layout hasSider>
     <Sider
+      breakpoint='lg'
+      // collapsible
+      collapsed={collapsed}
+      // collapsedWidth={0}
+      // onBreakpoint={ broken => {console.log(broken)}}
       theme="light"
       style={{
         overflow: 'auto',
@@ -44,21 +57,54 @@ const LayoutPage: React.FC = () => (
       }}
     >
       <div className="logo">
-        <CleverIcon />
+        {!collapsed && <CleverIcon />}
         <FitIcon />
       </div>
-      <Menu theme="light" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      <Menu
+        theme="light"
+        mode="inline"
+        defaultSelectedKeys={['4']}
+        items={items} />
+      <Button
+        type='text'
+        block
+        icon={<ExitIcon />}
+      >{!collapsed && 'Выход'}</Button>
     </Sider>
-    <Layout className="site-layout">
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div className="site-layout-background">
+    <Layout className="site-layout" style={{ width: 'auto' }}>
+      <Content className="site-layout-slider-button">
+        <Button
+          type="primary"
+          className='trigger'
+          onClick={toggleCollapsed}
+          style={{ marginBottom: 16, color: 'bisque', background: 'white' }}
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          size='small'>
+        </Button>
+      </Content>
+
+
+      <Content className="site-layout-background" style={{ margin: '24px 16px' }}>
+        <div
+          style={{
+            padding: 24,
+            minWidth: 320,
+            width: 100,
+            background: 'red',
+            borderRadius: '50',
+          }}
+        >
+
           <Outlet />
         </div>
+
+
       </Content>
-      <Footer >Смотреть отзывы</Footer>
+      <Footer>Смотреть отзывы</Footer>
     </Layout>
-  </Layout>
-);
+
+  </Layout>;
+
+};
 
 export { LayoutPage };
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './main-page.css';
+import { Header } from 'antd/lib/layout/layout';
 
 export const MainPage: React.FC = () => {
 
     return (
         <>
-            Главная
+            <Header className="site-layout-background" style={{ padding: 0 }}>
+                <h1>Главная</h1>
+            </Header>
         </>
     );
 };
